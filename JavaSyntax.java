99724985cbf0aca954317b05b39bc9408ceb4045
ArunPandyanVasam <you@example.com>
2023-06-04 18:58:34 -0400

    version1

---
+++ JavaSyntax.java
@@ -0,0 +1,7 @@
+package java_Syntax; //package
+
+public class JavaSyntax { //class
+    public static void main(String[] args) { //main method
+        System.out.println("Syntax"); //Statement
+    }
+}
